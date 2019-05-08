e4ab57f6f1fc5a47ca829612dc2e593185461661
ppd0705 <ppd0705@163.com>
2019-05-08 23:22:45 +0800

    add chapter 5

---
+++ 5-13-tail.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define DEFLINES 10
+#define LINES 100
+#define MAXLEN 100
+
+void error(char *);
+int get_line(char s[], int limit);
+
+int main(int argc, char *argv[])
+{
+    char *p;
+    char *buf;
+    char *bufend;
+    char line[MAXLEN];
+    char *lineptr[LINES];
+    int first, i, last, len, n, nlines;
+
+    if (argc == 1)
+        n = DEFLINES;
+    else if (argc == 2 && (*++argv)[0] == '-')
+        n = atoi(argv[0]+1);
+    else
+        error("usage: tail [-n]");
+
+    if (n < 1 || n > LINES)
+        n = LINES;
+    for (i = 0; i < LINES; i++)
+        lineptr[i] = NULL;
+    p = buf = (char *) malloc(LINES * MAXLEN);
+    if (p == NULL)
+        error("tail: cannot allocate buf");
+    bufend = buf + LINES * MAXLEN;
+    last = 0;
+    nlines = 0;
+    while (( len = get_line(line, MAXLEN)) > 0) {
+        if (p + len + 1 >= bufend)
+            p = buf;
+        lineptr[last] = p;
+        strcpy(lineptr[last], line);
+        if (++last >= LINES)
+            last = 0;
+        p += len + 1;
+        nlines++;
+    }
+    if (n > nlines)
+        n  = nlines;
+    first = last - n;
+    if (first < 0)
+        first += LINES;
+    for (i = first; n-- > 0; i = (i+1) % LINES)
+        printf("%s\n", lineptr[i]);
+}
+void error(char *s)
+{
+    printf("%s\n", s);
+    exit(1);
+}
+
+int get_line(char s[], int limit)
+{
+    int c, i;
+    for (i = 0; i < limit -1 && (c = getchar()) != EOF && c != '\n'; i++)
+        s[i] = c;
+    s[i] = '\0';
+    return i;
+}
+
